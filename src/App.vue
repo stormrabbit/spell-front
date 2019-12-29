bef7386da4175b78654aebd752e2d7f05073e43b
龙骑将杨影枫 <catwindboy@hotmail.com>
2019-12-29 23:44:14 +0800

    添加 flat 按钮和面包屑组件

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,17 @@
             v-icon 
           v-list-item-content
             v-list-item-title {{cs.nickname}}
-    v-app-bar(app,
-      color="indigo",
-      dark)
+    v-app-bar(app,color="indigo",dark)
       v-app-bar-nav-icon(@click.stop="drawer = !drawer") 
       v-toolbar-title {{cTitle}}
+      template(v-slot:extension)
+        v-btn(fab,color="primary",bottom,right,absolute, dark)
+          v-icon mdi-plus 
     v-content
+      div
+          v-breadcrumbs(:items="items",divider="-")
       v-container( class="fill-height",fluid)
+
         v-row(align="center", justify="center")
           v-col(class="text-center")
             v-btn(color="primary", @click="reload") reload
@@ -34,13 +38,22 @@ export default {
   data: () => ({
     drawer: false,
     chosenOne: {
-      nickname :'待选择',
+      nickname: "待选择"
     },
     classesList: [],
+    items: [{
+      text: '全部'
+    }, {
+      text: '一环'
+    },{
+      text: '二环'
+    },{
+      text: '三环'
+    }]
   }),
   computed: {
     cClassesList: function() {
-      return this.classesList  ;
+      return this.classesList;
     },
     cTitle: function() {
       return this.chosenOne.nickname;
@@ -48,8 +61,8 @@ export default {
   },
   methods: {
     chooseClass: function(clsObj) {
-        this.chosenOne = clsObj;
-        this.drawer = false;
+      this.chosenOne = clsObj;
+      this.drawer = false;
     },
     reload: function() {
       this.classesList = [];
@@ -60,9 +73,9 @@ export default {
           if (res) {
             _self.classesList = res.data;
             _self.chosenOne = _self.classesList[0];
-          } 
+          }
         })
-        .catch(() => _self.classesList = []);
+        .catch(() => (_self.classesList = []));
     }
   }
 };
