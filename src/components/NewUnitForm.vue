d3c18699b19375fc4707855e1a676b4bb09b1540
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-01-14 23:25:34 +0800

    新建

--- src/components/NewUnitForm.vue
+++ src/components/NewUnitForm.vue
@@ -1,33 +1,34 @@
 <!--  -->
 <template lang="pug">
     v-card
+        v-snackbar(v-model="snackbar", :timeout="timeout") {{ logTips }}
         v-card-title
             span(class="headline") 新建人物
         v-card-text
             v-container
                 v-row
                     v-col(cols="12", sm="6", md="4")
-                        v-text-field(label="姓名", required)
+                        v-text-field(label="姓名", required, v-model="cls.name")
+                    v-col(cols="12", sm="6", md="4", v-model="cls.name")
+                        v-select(label="种族", :items="['人类', '精灵', '矮人', '半精灵', '提夫林', '半兽人']", v-model="cls.race")
                     v-col(cols="12", sm="6", md="4")
-                        v-select(label="种族", :items="['人类', '精灵', '矮人', '半精灵', '提夫林', '半兽人']")
+                        v-select(label="职业", :items="clses", v-model="cls.cls")
                     v-col(cols="12", sm="6", md="4")
-                        v-select(label="职业", :items="clses")
+                        v-text-field(label="等级", required, v-model="cls.lvl")
                     v-col(cols="12", sm="6", md="4")
-                        v-text-field(label="等级", required)
+                        v-text-field(label="学派", required, v-model="cls.school")
                     v-col(cols="12", sm="6", md="4")
-                        v-text-field(label="学派", required)
-                    v-col(cols="12", sm="6", md="4")
-                        v-text-field(label="主属性", required)
+                        v-text-field(label="主属性", required, v-model="cls.value")
         v-card-actions
             v-spacer
             v-btn(color="primary", text, @click="closeCallBack") Close
-            v-btn(color="primary", text, @click="dialog = false") Save
+            v-btn(color="primary", text, @click="update") Save
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import qs from 'qs';
 export default {
   props: {
     clsList: Array,
@@ -38,7 +39,11 @@ export default {
   data() {
     //这里存放数据
     return {
-      dialog: true
+      dialog: true,
+      cls: {},
+      logTips: '',
+      timeout: 2000,
+      snackbar: false,
     };
   },
   //监听属性 类似于data概念
@@ -50,7 +55,18 @@ export default {
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+      update: function() {
+        const _self = this;
+        if(_self.closeCallBack) {
+            _self.closeCallBack();
+        }
+        this.$axios.post(`http://localhost:3000/charactor/create`, qs.stringify(_self.cls)).then( res => {
+            _self.snackbar = true;
+            this.logTips = res;
+        });
+      }
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
