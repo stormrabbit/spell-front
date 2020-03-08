0ffe1b4b439bf6b375c5d0e13be57999ddc04d70
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-03-08 23:26:41 +0800

    fix

--- src/containers/HomePage.vue
+++ src/containers/HomePage.vue
@@ -1,7 +1,9 @@
 <!-- -->
 <template lang="pug">
   div(class="fill-height",fluid)
-    v-chip(class="ma-2" v-for="(spell, index) in curSpells", :key="index" close) {{spell.nickname}}
+    v-chip(class="ma-2" v-for="(spell, index) in curSpells", :key="index" close @click="() => {onClick(index)}") 
+      span(v-if="parseInt(spell.status)") {{`${spell.nickname}`}} 
+      s(v-else) {{spell.nickname}}
     //- v-card(dark,style="width: 100%;margin-top: 12px;", color="primary", v-for="(spell, index) in curSpells", :key="index", @click="() => {onClick(index)}" :disabled="!parseInt(spell.status)" )
     //-   v-card-title(class="headline") 
     //-     span(v-if="parseInt(spell.status)") {{`${spell.nickname}`}} 
