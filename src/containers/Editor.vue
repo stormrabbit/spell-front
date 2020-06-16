013aa1a7f953beced2ce62bb6d24f78a8a25df39
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-06-16 23:48:58 +0800

    feat: 属性,不好弄

--- src/containers/Editor.vue
+++ src/containers/Editor.vue
@@ -1,75 +1,92 @@
 <template lang="pug">
   v-app(id="inspire")
-    v-stepper(v-model="e6" vertical)
-      v-stepper-step(:complete="e6 > 1" step="1") 选择种族
+    v-stepper(v-model="step" vertical)
+      v-stepper-step(:complete="step > 1" step="1") 选择种族
       v-stepper-content(step="1")
-        v-card(class="mb-12" color="grey lighten-3"  style="background: #fff")
+        v-card(class="mb-12" color="grey lighten-3")
           v-card-text
             v-radio-group(row)
-              v-radio(v-for="(race, index) in races" :label="race" :key="index")
+              v-row(align="center" )
+                v-col(cols="2" v-for="(race, index) in races" :key="index" style="text-align: center;")
+                  v-radio(:label="race" )
           v-card-actions
-            v-btn(text @click="e6 = 2" ) 确认
-      v-stepper-step(:complete="e6 > 2" step="2") 选择职业
+            v-btn(text @click="step = 2" ) 确认
+      v-stepper-step(:complete="step > 2" step="2") 选择职业
       v-stepper-content(step="2")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-text
             v-radio-group(row)
-              v-radio(v-for="(clazz, index) in classes" :label="clazz" :key="index")
+              v-row(align="center" )
+                v-col(cols="2" v-for="(clazz, index) in classes" :key="index" style="text-align: center;")
+                  v-radio( :label="clazz")
           v-card-actions
-            v-btn(text @click="e6 = 3") 确认
-            v-btn(text @click="e6 = 1") 返回
-      v-stepper-step(:complete="e6 > 3" step="3") 属性值
+            v-btn(text @click="step = 3") 确认
+            v-btn(text @click="step = 1") 返回
+      v-stepper-step(:complete="step > 3" step="3") 属性值
       v-stepper-content(step="3")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-title 剩余点数：27
           v-card-text
-            div
+            v-row
+              v-col(cols="2")
+                v-text-field(label="力量" v-model="abilities.str" outlined)
+              v-col(cols="2")
+              v-col(cols="2")
+                v-chip(class="ma-2" color="red" text-color="white") -1
           v-card-actions
-            v-btn(text @click="e6 = 4") 确认
-            v-btn(text @click="e6 = 2") 返回
-      v-stepper-step(:complete="e6 > 4" step="4") 选择阵营
+            v-btn(text @click="step = 4") 确认
+            v-btn(text @click="step = 2") 返回
+      v-stepper-step(:complete="step > 4" step="4") 选择阵营
       v-stepper-content(step="4")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-text
             v-radio-group(row)
               v-radio(v-for="(alignment, index) in alignments" :label="alignment" :key="index")
           v-card-actions
-            v-btn(text @click="e6 = 5") 确认
-            v-btn(text @click="e6 = 3") 返回
-      v-stepper-step(:complete="e6 > 5" step="5") 选择背景
+            v-btn(text @click="step = 5") 确认
+            v-btn(text @click="step = 3") 返回
+      v-stepper-step(:complete="step > 5" step="5") 选择背景
       v-stepper-content(step="5")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-text
             v-radio-group(row)
               v-radio(v-for="(background, index) in backgrounds" :label="background" :key="index")
           v-card-actions
-            v-btn(text @click="e6 = 6") 确认
-            v-btn(text @click="e6 = 4") 返回
-      v-stepper-step(:complete="e6 > 6" step="6") 选择技能
+            v-btn(text @click="step = 6") 确认
+            v-btn(text @click="step = 4") 返回
+      v-stepper-step(:complete="step > 6" step="6") 选择技能
       v-stepper-content(step="6")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-text
             div(style="display: flex;align-items: center;")
               v-checkbox(v-for="(skill, index) in skills" :label="skill" :key="index")
           v-card-actions
-            v-btn(text @click="e6 = 7") 确认
-            v-btn(text @click="e6 = 5") 返回
-      v-stepper-step(:complete="e6 > 7" step="7") 选择专长
+            v-btn(text @click="step = 7") 确认
+            v-btn(text @click="step = 5") 返回
+      v-stepper-step(:complete="step > 7" step="7") 选择专长
       v-stepper-content(step="7")
         v-card(color="grey lighten-3" class="mb-12")
           v-card-text
             div(style="display: flex;align-items: center;")
               v-checkbox(v-for="(feat, index) in feats" :label="feat" :key="index")
           v-card-actions
-            v-btn(text @click="e6 = 8") 确认
-            v-btn(text @click="e6 = 6") 返回    
+            v-btn(text @click="step = 8") 确认
+            v-btn(text @click="step = 6") 返回    
 </template>
 
 <script>
     export default {
         data () {
             return {
-                e6: 1,
+                step: 3,
+                abilities: {
+                  str: 8,
+                  dex: 8,
+                  con: 8,
+                  wis: 8,
+                  int: 8,
+                  cha: 8
+                },
                 races:["矮人","精灵","半身人","人类","龙裔","侏儒","半精灵","半兽人","提夫林"],
                 classes: ["野蛮人","吟游诗人","牧师","德鲁伊","战士","武僧","圣武士","游侠","游荡者","邪术师","法师"],
                 alignments: ["守序善良","中立善良","混乱善良","守序中立","绝对中立","混乱中立","守序邪恶","中立邪恶","混乱邪恶"],
@@ -80,3 +97,9 @@
         },
     }
 </script>
+
+<style scoped>
+/deep/ .v-input--selection-controls .v-input__control {
+  width: 100%;
+}
+</style>>
