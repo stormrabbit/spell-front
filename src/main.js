e3165d483d669f9aa979de9fdb43cb9f54acb601
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-08-24 21:16:08 +0800

    feature:chart 初步

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import vuetify from './plugins/vuetify';
 import store from './store';
 import router from './router';
 import Sender from './slamdunk/sender';
+// eslint-disable-next-line no-irregular-whitespace
+import VCharts from 'v-charts';
 import {
   error2Event
 } from './slamdunk/utils/errorUtils';
+
+Vue.use(VCharts);
+
 Vue.config.productionTip = false
 
 Vue.config.errorHandler = (err) => {
