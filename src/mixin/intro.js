2b7c221aa5ba357a758d2b3f977e5c3920d0b9d3
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-09-13 23:15:05 +0800

    feature: intro 优化

--- src/mixin/intro.js
+++ src/mixin/intro.js
@@ -1,5 +1,31 @@
 export default {
-    data:() => ({
-        demo: 'intros'
-    })
+    data: () => ({
+        demo: 'intros',
+        introSteps: []
+    }),
+    methods: {
+        initIntro() {
+            this.intro = this.$intro();
+            this.intro.setOptions({
+                tooltipPosition: 'right',
+                disableInteraction: true,
+                showStepNumbers: false,
+                highlightClass: 'intro__highlight',
+                hidePrev: true,
+                hideNext: true,
+                skipLabel: '退出引导',
+                nextLabel: '下一步',
+                doneLabel: '退出引导',
+                overlayOpacity: 0.5,
+                exitOnOverlayClick: false,
+                scrollPadding: 0,
+                scrollToElement: false,
+            });
+            this.introSteps.forEach((step) => {
+                let element = this.$el.querySelector(step.selector);
+                const introStep = { ...step, element: element }
+                this.intro.addStep(introStep)
+              })
+        }
+    },
 }
